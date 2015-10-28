0be8ffd3a96a88abfa89ec3361bfcdcce9ae5075
jorgeazorin <jorgeazorin@gmail.com>
2015-10-28 13:09:26 +0100

    Ahora se entiende y coge todos los tweets de un usuario, modularizado y tal

--- twitterClient/twitterClient.cpp
+++ twitterClient/twitterClient.cpp
@@ -6,6 +6,13 @@
 #include <tr1/unordered_map>
 
 using namespace std;
+///////////////////////////////////////////////////////////////
+//Variables globales
+//////////////////////////////////////////////////////////////
+string ultimotweet;
+
+
+
 
 
 //////////////////////////////////////////////////////////////
@@ -23,6 +30,13 @@ using namespace std;
 		  return internal;
 	}
 
+
+
+
+
+//////////////////////////////////////////////////////////////
+//Modulo para pasar los meses a numeros
+/////////////////////////////////////////////////////////////
 	string MesToNum(string mes){
 		if(mes=="Jan")
 			return "01";
@@ -52,11 +66,82 @@ using namespace std;
 			return "00";
 	}
 
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////
+//Clase Tweet
+/////////////////////////////////////////////////////////////
 	class Tweet {
   		public:
-  			int fecha;
-  			string texto;
+  			int fecha=0;
+  			string texto="";
+
     };
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////
+//Modulo para obtener fecha y texto de un tweet
+/////////////////////////////////////////////////////////////
+    Tweet StringToTweet(string t){
+        bool ya2=false;
+        bool ya3=true;
+    	Tweet tweet;
+    	if(t.length()>35){
+
+			
+    			///////////////////////////////////////////////
+    			///Obtener la fecha del tweet//////////////////
+    			///////////////////////////////////////////////
+
+			 	vector<string> fecha = split(t.substr(3,30), ' ');
+			 	string fechastring=fecha[2]+MesToNum(fecha[1])+fecha[5];
+			 	int fechaint = atoi(fechastring.c_str());   
+			 	tweet.fecha=fechaint;    
+
+
+
+				///////////////////////////////////////////////
+    			///Obtener el texto del tweet//////////////////
+    			///////////////////////////////////////////////
+
+    			tweet.texto=t.substr(t.find("text")+7,t.find("source")-t.find("text")-10);
+
+
+
+
+    			///////////////////////////////////////////////
+    			///Obtener el id del tweet/////////////////////
+    			///////////////////////////////////////////////
+
+    			ultimotweet=t.substr(t.find("id")+4,t.find("id_str")-t.find("id")-6);
+
+		}
+		return tweet;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //////////////////////////////////////////////////////////////
 //MAIN
@@ -64,13 +149,16 @@ using namespace std;
 int main( int argc, char* argv[] )
 {
 
-	//Crear objeto para twitter e iniciar sesion
+
+
+    ///////////////////////////////////////////////////////
+    //Inicializar la libreria twitcurl/////////////////////
+    ///////////////////////////////////////////////////////
 
     twitCurl twitterObj;
     std::string tmpStr, tmpStr2;
     std::string replyMsg;
     char tmpBuf[2048];
-
     twitterObj.setTwitterUsername( "jorgeazorin" );
     twitterObj.setTwitterPassword( "179832" );
     memset( tmpBuf, 0, 2048 );
@@ -79,68 +167,75 @@ int main( int argc, char* argv[] )
     replyMsg = "";					
 
 
-    //Crear un archivo con los resultados
+
+
+
+
+
+    ///////////////////////////////////////////////////////
+    //Crear un archivo con los resultados//////////////////
+    ///////////////////////////////////////////////////////
+
+
     ofstream Fichero("datos.csv");         //Opening file to print info to
     Fichero << "Time Force(N/m)"<<";"<<"Otra cosa" << endl;          //Headings for file
 
-    //Obtener los tweets de un usuario
-    bool tweetscompletos=false;
-    string ultimotweet="";
-    while(!tweetscompletos){
-    	tweetscompletos=true;
-   							 //trimUser //retweets // nº //usuario//esusuarioID//ultimotweet
-	    if( twitterObj.timelineUserGet(  true, false,1000000,"encarnaamoros",false,ultimotweet) ){
-	        twitterObj.getLastWebResponse( replyMsg );
-	        string a=replyMsg.c_str();
-	        vector<string> sep = split(a, ',');
-	        bool ya=false;
-	        bool ya2=false;
-	        bool ya3=true;
-		   	for(string t : sep){
-			   	if(t.length()>3){
-		    		 if(t.substr (2,10)=="created_at" ){
-		    		 	vector<string> fecha = split(t.substr(15,t.length()-16), ' ');
-		    		 	string fechastring=fecha[2]+MesToNum(fecha[1])+fecha[5];
-		    		 	int fechaint = atoi(fechastring.c_str());   
-		    		 	Fichero <<fechastring<<";";                             
-		    		 	ya=true;
-		    		 	ya3=true;
-		    		 	tweetscompletos=false;
-		    		 }else if(ya3==true && t.substr (1,2)== "id"){
-		     			ultimotweet=t.substr(5,t.length()-5);
-		     			ya3=false;
-		     			 
-					}else
-						ya3=false;
-		    		 if( t.substr (1,4)== "text" && ya==true && t.substr (1,4)== "text"){
-		    		 	Fichero <<t.substr(8,t.length()-9); 
-		    		 	ya2=true;
-		     		}
-		     		if(t.substr(1,6)=="source" && ya==true){
-		     			Fichero <<endl;
-		     			ya2=false;
-		     			ya=false;
-		     		}
-		     		if(t.substr(1,6)=="source" && ya==true){
-		     			Fichero <<endl;
-		     			ya2=false;
-		     			ya=false;
-		     		}
-                   		     
-		     		if(ya2==true){
-		       			Fichero<<t;
-		     		}
-		     	}else
-					tweetscompletos=true;
-			}
-	    }else{
-	    	printf( "\nNo va\n" );
-	    	tweetscompletos=true;
-	        twitterObj.getLastCurlError( replyMsg );
-	    }  
-	 }
 
-      
+
+
+
+
+    ///////////////////////////////////////////////////////
+    //Obtener los tweets de un usuario/////////////////////
+    ///////////////////////////////////////////////////////
+
+    bool tweetscompletos=false;
+
+    ///Mientas la respuesta no sea un json vacio va a ir pidiendo tweets desde el ultimo recibido
+    while(!tweetscompletos){
+    	tweetscompletos=true;					 
+	    if( twitterObj.timelineUserGet(true, false,1000000,"jorgeazorin",false,ultimotweet)){//trimUser //retweets // nº //usuario//esusuarioID//ultimotweet
+	        twitterObj.getLastWebResponse( replyMsg );
+	        string respuesta=replyMsg.c_str();
+
+	        //Veces que encuentra un tweet en el json si encuentra menos de 3 es seguro que ya no quedan tweets por mirar
+	        int repeticiones=0;
+
+	        if(respuesta.length()>10){
+	        		tweetscompletos=false;
+	        		cout<<"Mirando tweets desde "<<ultimotweet<<" de "<<"jorgeazorin"<<endl;
+
+			        //Dividir el json de respuesta entre tweets//////////////////////////////////////////////
+			        Tweet tweet;
+			        size_t pos = 0;
+					string token;
+					while ((pos = respuesta.find("created_at")) !=string::npos) {
+						repeticiones++;
+					    token = respuesta.substr(0, pos);
+					    //Quitamos el tweet original del retwiteado (esto no borra los rt si no que por culpa del json el tweet aparece 2 veces cuando esta retuiteado, deja 1)
+					    if(!token.find("retweeted_status")!=string::npos){
+					    	tweet=StringToTweet(token);
+					    	Fichero<<tweet.fecha<<";"<<tweet.texto<<endl;
+					    }
+					    respuesta.erase(0, pos + 10);
+					}
+
+
+
+	        		//Veces que encuentra un tweet en el json si encuentra menos de 3 es seguro que ya no quedan tweets por mirar
+					if(repeticiones<3)
+						tweetscompletos=true;;
+
+	    	}else{
+		    	printf( "\nSe acabó\n" );
+		    	tweetscompletos=true;
+		       // twitterObj.getLastCurlError( replyMsg );
+	    	}  
+	 	}
+     }
+
+
+
 
      //Cerrar fichero
      Fichero.close();
