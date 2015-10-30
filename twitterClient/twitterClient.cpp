f5754d0b8f8dffe162191cc8ea8e1ccfda3b81af
jorgeazorin <jorgeazorin@gmail.com>
2015-10-30 23:05:40 +0100

    acentos y mayusculas controlados y busqueda en fechas tambien se guarda en fechas las veces que aparece una palabra y en el fichero de salida aparecen las fechas con la cantidad de veces que ha salido la palabra

--- twitterClient/twitterClient.cpp
+++ twitterClient/twitterClient.cpp
@@ -3,18 +3,35 @@
 #include <vector>
 #include <string>
 #include <sstream>
+#include <unordered_map>
+#include <ctype.h>
 using namespace std;
+
+
+
+///////////////////////////////////////////////////////////////
+//Constantes globales
+//////////////////////////////////////////////////////////////
+const int MAX_USUARIOS_A_MIRAR=20;
+const int MAX_BUSCARAMIGOS=50; //maximo de veces que se buscan amigos si es 1 solo buscara los amigos de jorgeazorin si es mas busca tambien los amigos de los amigos de jorgeazorin
+const int USUARIO_INICIO=144533310; //Este es @jorgeazorin
+string palabrabuscada="fútbol";
+
+
+
 
 
 ///////////////////////////////////////////////////////////////
 //Variables globales
 //////////////////////////////////////////////////////////////
 string ultimotweet;
-int usuariosMirados=0;
+unsigned int usuariosMirados=0;
+unsigned int tweetsMirados=0;
+unsigned int vecesQueApareceLaPalabra=0;
 vector<int> UsuariosSinMirar;
 vector<int> UsuariosMirados;
-const int MAX_USUARIOS_A_MIRAR=10;
-const int USUARIO_INICIO=144533310;
+unordered_map<int, int> VecesPorFecha;
+
 
 
 
@@ -26,8 +43,9 @@ class Tweet {
 	public:
 		int fecha=0;
 		string texto="";
-
 };
+
+
 
 
 
@@ -42,7 +60,6 @@ vector<string> split(string str, char delimiter) {
 	  while(getline(ss, tok, delimiter)) {
 	    internal.push_back(tok);
 	  }
-	  
 	  return internal;
 }
 
@@ -53,18 +70,12 @@ vector<string> split(string str, char delimiter) {
 //Modulo para pasar los meses a numeros
 /////////////////////////////////////////////////////////////
 string MesToNum(string mes){
-	if	   (mes=="Jan") return "01";
-	else if(mes=="Feb") return "02";
-	else if(mes=="Mar") return "03";
-	else if(mes=="Apr") return "04";
-	else if(mes=="May") return "05";
-	else if(mes=="Jun") return "06";
-	else if(mes=="Jul") return "07";
-	else if(mes=="Aug") return "08";
-	else if(mes=="Sep") return "09";
-	else if(mes=="Oct") return "10";
-	else if(mes=="Nov") return "11";
-	else if(mes=="Dec") return "12";
+	if	   (mes=="Jan") return "01"; 	else if(mes=="Feb") return "02";
+	else if(mes=="Mar") return "03";	else if(mes=="Apr") return "04";
+	else if(mes=="May") return "05";	else if(mes=="Jun") return "06";
+	else if(mes=="Jul") return "07";	else if(mes=="Aug") return "08";
+	else if(mes=="Sep") return "09";	else if(mes=="Oct") return "10";
+	else if(mes=="Nov") return "11";	else if(mes=="Dec") return "12";
 	else         		return "00";
 }
 
@@ -77,7 +88,7 @@ string MesToNum(string mes){
 Tweet StringToTweet(string t){
 	Tweet tweet;
 	if(t.length()>35){
-
+		tweetsMirados++;
 		///Obtener la fecha del tweet//////////////////
 	 	vector<string> fecha = split(t.substr(3,30), ' ');
 	 	string fechastring=fecha[2]+MesToNum(fecha[1])+fecha[5];
@@ -97,6 +108,36 @@ Tweet StringToTweet(string t){
 
 
 
+//////////////////////////////////////////////////////////////
+//Modulo para reemplazar todas las veces un texto de otro
+/////////////////////////////////////////////////////////////
+void replaceAll(string& str, const string& from, const string& to) {
+    if(from.empty())
+        return;
+    size_t start_pos = 0;
+    while((start_pos = str.find(from, start_pos)) != std::string::npos) {
+        str.replace(start_pos, from.length(), to);
+        start_pos += to.length(); // In case 'to' contains 'from', like replacing 'x' with 'yx'
+    }
+}
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////
+//Modulo para quitar los acentos de un texto
+/////////////////////////////////////////////////////////////
+void QuitarAcentos(string& texto){
+	//minusculas en json				//mayusculas en json				/minusculas	en unicode		/mayusculas en unicode
+	replaceAll(texto,"\\u00e1","a");	replaceAll(texto,"\\u00c1","a");	replaceAll(texto,"á","a");	replaceAll(texto,"Á","a");
+	replaceAll(texto,"\\u00e9","e");	replaceAll(texto,"\\00c9","e");		replaceAll(texto,"é","e");	replaceAll(texto,"É","e");
+	replaceAll(texto,"\\u00ed","i");	replaceAll(texto,"\\u00cd","i");	replaceAll(texto,"í","i");	replaceAll(texto,"Í","i");
+	replaceAll(texto,"\\u00f3","o");	replaceAll(texto,"\\u00d3","o");	replaceAll(texto,"ó","o");	replaceAll(texto,"Ó","o");
+	replaceAll(texto,"\\u00fa","u");	replaceAll(texto,"\\u00dA","u");	replaceAll(texto,"ú","u");	replaceAll(texto,"Ú","u");
+}
+
 
 //////////////////////////////////////////////////////////////
 //                        MAIN                            ////
@@ -104,8 +145,7 @@ Tweet StringToTweet(string t){
 int main( int argc, char* argv[] )
 {
 
-
-
+	
     ///////////////////////////////////////////////////////
     //Inicializar la libreria twitcurl/////////////////////
     ///////////////////////////////////////////////////////
@@ -125,10 +165,14 @@ int main( int argc, char* argv[] )
 
     //Crear un archivo con los resultados//////////////////
     ofstream Fichero("datos.csv");         //Opening file to print info to
-    Fichero << "Time Force(N/m)"<<";"<<"Otra cosa" << endl;          //Headings for file
+    Fichero << "Fecha"<<";"<<"Numero de tweets" << endl;          //Headings for file
 
+    //pasamos a minusculas la palabra a buscar
+	for(int i = 0; i<palabrabuscada.length(); i++)
+		palabrabuscada[i] = tolower(palabrabuscada[i]);
 
-
+	//le quitamos los acentos a la palabra
+	QuitarAcentos(palabrabuscada);
 
 
 
@@ -138,7 +182,8 @@ int main( int argc, char* argv[] )
 
     //Añadimos el usuario Inicial a la lista
     UsuariosSinMirar.push_back(USUARIO_INICIO);
-
+    if(UsuariosSinMirar.empty())
+    cout<<"No hay usuarios sin mirar!!!!!!!!!"<<endl;
     while(usuariosMirados<MAX_USUARIOS_A_MIRAR && !UsuariosSinMirar.empty()){
     	usuariosMirados++;
 
@@ -161,6 +206,8 @@ int main( int argc, char* argv[] )
 		        twitterObj.getLastWebResponse( replyMsg );
 		        string respuesta=replyMsg.c_str();
 
+
+
 		        //Veces que encuentra un tweet en el json si encuentra menos de 3 es seguro que ya no quedan tweets por mirar
 		        int repeticiones=0;
 
@@ -178,7 +225,25 @@ int main( int argc, char* argv[] )
 						    //Quitamos el tweet original del retwiteado (esto no borra los rt si no que por culpa del json el tweet aparece 2 veces cuando esta retuiteado, deja 1)
 						    if(!token.find("retweeted_status")!=string::npos){
 						    	tweet=StringToTweet(token);
-						    	Fichero<<tweet.fecha<<";"<<tweet.texto<<endl;
+
+
+						    	//pasamos a minusculas el tweet y le quitamos los acentos
+						    	for(int i = 0; i<tweet.texto.length(); i++)
+      								tweet.texto[i] = tolower(tweet.texto[i]);
+      							QuitarAcentos(tweet.texto);
+
+
+						    	if(tweet.texto.find(palabrabuscada)!=string::npos){
+						    			vecesQueApareceLaPalabra++;
+
+						    			//Sumar a la fecha 1 si lo encuentra
+						    			unordered_map<int,int>::const_iterator got = VecesPorFecha.find (tweet.fecha);
+  										if (got == VecesPorFecha.end() ){
+						    				pair<int,int> parfehcanumerotweets (tweet.fecha,1);
+						    				VecesPorFecha.insert(parfehcanumerotweets);
+					    				}else
+						    				VecesPorFecha.at(tweet.fecha)=VecesPorFecha.at(tweet.fecha)+1;
+						    		}
 						    }
 						    respuesta.erase(0, pos + 10);
 						}
@@ -205,8 +270,8 @@ int main( int argc, char* argv[] )
 
 	    replyMsg = "";
 	    tmpStr = "techcrunch";
-	    if( twitterObj.friendsIdsGet( "", to_string(UsuarioID),true ) )
-	    {
+	    if( twitterObj.friendsIdsGet( "", to_string(UsuarioID),true ) && usuariosMirados<=MAX_BUSCARAMIGOS){
+	    	cout<<"Buscando amigos"<<endl;
 	        //Pedimos los ids de los usuarios y los separamos en un vector/////////
 	        twitterObj.getLastWebResponse( replyMsg );
 	        string respuesta=replyMsg.c_str();
@@ -214,12 +279,13 @@ int main( int argc, char* argv[] )
 	        respuesta=respuestas[0].substr(8,respuestas[0].length()-8);
 	        vector<string> ids= split(respuesta,',');
 
-
+	       
 	        //Miramos si lo hemos mirado y si esta por mirar/////////////////////
 	        bool EstaEnUsuariosSinMirar=false;
 	        bool EstaEnUsuariosMirados=false;
 
 	        for (int i=0; i<ids.size(); i++){
+	        	
 	        	int EnteroId=atoi(ids[i].c_str());
 	        	for (int t=0; t<UsuariosSinMirar.size(); t++)
 	        		if(EnteroId==UsuariosSinMirar[t])
@@ -231,21 +297,34 @@ int main( int argc, char* argv[] )
 	        	if(!EstaEnUsuariosMirados && !EstaEnUsuariosSinMirar)
 	        	 	UsuariosSinMirar.push_back(EnteroId);	
 			}
-
-
-
-
-	      //  cout<<respuesta;
-	       // printf( "\ntwitterClient:: twitCurl::friendsIdsGet web response:\n%s\n", replyMsg.c_str() );
 	    }
-	    else
-	    {
-	        twitterObj.getLastCurlError( replyMsg );
-	        printf( "\ntwitterClient:: twitCurl::friendsIdsGet error:\n%s\n", replyMsg.c_str() );
-	    }
-
-
  	}
+    
+
+    string fechabruto="";
+    string fechalimpia="";
+
+    //Mostrar resultados por pantalla
+    for (auto& x: VecesPorFecha){
+    	fechabruto=to_string(x.first);
+
+    	//Si el dia es menor de 10 le ponemos un 0 delante
+    	if(fechabruto.length()<8)
+    		fechabruto="0"+fechabruto;
+
+    	//ponemos la fecha con /
+    	fechalimpia=fechabruto.at(0);	fechalimpia+=fechabruto.at(1);	fechalimpia+="/";  	//Dia
+    	fechalimpia+=fechabruto.at(2);	fechalimpia+=fechabruto.at(3);	fechalimpia+="/";	//Mes
+    	fechalimpia+=fechabruto.at(4);	fechalimpia+=fechabruto.at(5); 	fechalimpia+=fechabruto.at(6); fechalimpia+=fechabruto.at(7); //Año
+
+
+    	Fichero<<fechalimpia<<";"<< x.second<<endl;
+    }
+
+    cout<<endl<<endl<<"Total tweets mirados "<<tweetsMirados << " de "<< usuariosMirados <<" usuarios"<<endl;
+    cout<<"la palabra "<<palabrabuscada<<" aparece "<<vecesQueApareceLaPalabra<<" veces"<<endl;
+   
+
     //Cerrar fichero
     Fichero.close();
 }
